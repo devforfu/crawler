55a6c7f8c355b75cba5552f646094b0bb9a63033
devforfu <developer.z@outlook.com>
2018-12-23 12:38:11 +0500

    Example of exponential waiting

--- count.go
+++ count.go
@@ -4,9 +4,12 @@ import (
     "./src/fetch"
     "fmt"
     "golang.org/x/net/html"
+    "log"
+    "net/http"
     "os"
     "strconv"
     "strings"
+    "time"
 )
 
 type Counts = struct { words, images int }
@@ -20,9 +23,14 @@ func main() {
     counts := map[string]Counts {}
 
     for i, url := range urls {
-        fmt.Printf("[%*d of %d] %s\n", width, i+1, total, url)
-        result := fetch.Fetch(url)
+        log.Printf("[%*d of %d] %s\n", width, i+1, total, url)
+        err := WaitForServer(url)
         truncatedUrl := url[:minInt(30, len(url))]
+        if err != nil {
+            failed[url] = fmt.Sprintf("%s", err)
+            continue
+        }
+        result := fetch.Fetch(url)
         if result.Failed() {
             failed[truncatedUrl] = *result.Error
             continue
@@ -36,11 +44,11 @@ func main() {
     }
 
     for url, err := range failed {
-        fmt.Printf("%-30s | error: %s\n", url, strings.Trim(err, "\n\t\r"))
+        log.Printf("%-30s | error: %s\n", url, strings.Trim(err, "\n\t\r"))
     }
 
     for url, count := range counts {
-        fmt.Printf("%-30v | words: \t%d\timages:\t%d\n", url, count.words, count.images)
+        log.Printf("%-30v | words: \t%d\timages:\t%d\n", url, count.words, count.images)
     }
 }
 
@@ -74,4 +82,20 @@ func countWordsAndImages(n *html.Node) (nWords, nImages int) {
         nImages += i
     }
     return nWords, nImages
+}
+
+// WaitForServer attempts to contact the server of a URL.
+// It tries for one minute using exponential back-off.
+// It reports an error if all attempts fail.
+// Reference: gopl.io/ch5/wait
+func WaitForServer(url string) error {
+    const timeout = 1 * time.Minute
+    deadline := time.Now().Add(timeout)
+    for tries := 0; time.Now().Before(deadline); tries++ {
+        _, err := http.Head(url)
+        if err == nil { return nil }
+        log.Printf("server not responding (%s); retrying...", err)
+        time.Sleep(time.Second << uint(tries))
+    }
+    return fmt.Errorf("server %s failed to responsd after %s", url, timeout)
 }
