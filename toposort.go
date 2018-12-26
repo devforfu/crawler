989367881c13d20748356cdceb0e1fc39676e140
devforfu <developer.z@outlook.com>
2018-12-26 22:44:08 +0500

    Toposort layout

---
+++ toposort.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+var courses = map[string][]string {
+    "algorithms": {"data structures"},
+    "calculus": {"linear algebra"},
+    "compilers": {
+        "data structures",
+        "formal languages",
+        "computer organization",
+    },
+    "data structures": {"discrete math"},
+    "databases": {"data structures"},
+    "discrete math": {"intro to programming"},
+    "formal languages": {"discrete math"},
+    "networks": {"operating systems"},
+    "operating systems": {"data structures", "computer organization"},
+    "programming languages": {"data structures", "computer organization"},
+}
+
+func main() {
+    index := 1
+    for course, _ := range courses {
+        fmt.Printf("%d:\t%s\n", index, course)
+        index += 1
+    }
+}
