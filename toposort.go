fbb3c0d02f77126468c4b6df25fda762986fad81
devforfu <developer.z@outlook.com>
2018-12-31 14:59:43 +0500

    Toposort formatting

--- toposort.go
+++ toposort.go
@@ -3,19 +3,15 @@ package main
 import "fmt"
 
 var courses = map[string][]string {
-    "algorithms": {"data structures"},
-    "calculus": {"linear algebra"},
-    "compilers": {
-        "data structures",
-        "formal languages",
-        "computer organization",
-    },
-    "data structures": {"discrete math"},
-    "databases": {"data structures"},
-    "discrete math": {"intro to programming"},
-    "formal languages": {"discrete math"},
-    "networks": {"operating systems"},
-    "operating systems": {"data structures", "computer organization"},
+    "algorithms":            {"data structures"},
+    "calculus":              {"linear algebra"},
+    "compilers":             {"data structures", "formal languages", "computer organization"},
+    "data structures":       {"discrete math"},
+    "databases":             {"data structures"},
+    "discrete math":         {"intro to programming"},
+    "formal languages":      {"discrete math"},
+    "networks":              {"operating systems"},
+    "operating systems":     {"data structures", "computer organization"},
     "programming languages": {"data structures", "computer organization"},
 }
 
