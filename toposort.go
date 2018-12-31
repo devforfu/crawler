71f84c38468fbd1f566a50773309401ed24fa195
devforfu <developer.z@outlook.com>
2018-12-31 15:46:51 +0500

    Toposort with Sort experiments

--- toposort.go
+++ toposort.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "sort"
+)
 
 var courses = map[string][]string {
     "algorithms":            {"data structures"},
@@ -15,10 +18,60 @@ var courses = map[string][]string {
     "programming languages": {"data structures", "computer organization"},
 }
 
-func main() {
-    index := 1
-    for course, _ := range courses {
-        fmt.Printf("%d:\t%s\n", index, course)
-        index += 1
+type DependencyList struct {
+    Mapping map[string][]string
+    sortedKeys []string
+}
+
+func (d DependencyList) Len() int {
+    return len(d.Mapping)
+}
+
+func (d DependencyList) Less(i, j int) bool {
+    keys := d.SortedKeys()
+    return len(d.Mapping[keys[i]]) < len(d.Mapping[keys[j]])
+}
+
+func (d DependencyList) Swap(i, j int) {
+    keys := d.SortedKeys()
+    keys[i], keys[j] = keys[j], keys[i]
+}
+
+func (d DependencyList) SortedKeys() []string {
+    var keys []string
+    if d.sortedKeys == nil {
+        for key := range d.Mapping {
+            keys = append(keys, key)
+        }
+        d.sortedKeys = keys
     }
+    keys = d.sortedKeys
+    return keys
+}
+
+func main() {
+    for i, course := range topoSort(courses) {
+        fmt.Printf("%d:\t%s\n", i, course)
+    }
+}
+
+func topoSort(m map[string][]string) []string {
+    var order []string
+    seen := make(map[string]bool)
+    var visitAll func(items []string)
+
+    visitAll = func(items []string) {
+        for _, item := range items {
+            if !seen[item] {
+                seen[item] = true
+                visitAll(m[item])
+                order = append(order, item)
+            }
+        }
+    }
+
+    deps := DependencyList{Mapping: m}
+    sort.Sort(deps)
+    visitAll(deps.SortedKeys())
+    return order
 }
